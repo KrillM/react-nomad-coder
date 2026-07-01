5ecbfb8d56ef0a5fb2f0d80b0ff2e5f957bc0816
krille <mks160615@gmail.com>
2026-07-01 23:36:42 +0900

    add memo

--- src/02_props/A_props_c.js
+++ src/02_props/A_props_c.js
@@ -1,7 +1,19 @@
-export default function A_props_c ({numberValue, value}){
+import { memo } from "react";
+
+const A_props_c = ({numberValue, value}) => {
+
+    console.log("Render: ", value);
+
     return(
         <>
             { value==="add" ? <button onClick={numberValue}>+</button> : <button onClick={numberValue}>-</button>}
         </>
     )
 } 
+
+/*
+    부모 컴포넌트가 변경될 때마다 자녀의 컴포넌트를 새로 받는다.
+    자녀 컴포넌트가 많다면 이 과정이 매우 길다.
+    따라서 기존의 컴포넌트를 다시 참조하지 않기 위해 memo()를 사용한다.
+*/
+export default memo(A_props_c);
