e70841bc27195e6ca4beae7e3b30fe5e8a02c7ba
krille <mks160615@gmail.com>
2026-07-07 22:00:19 +0900

    fetch

---
+++ src/04_practice/C_TodoList.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const C_TodoList = () => {
+
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]); 
+
+    useEffect(()=>{
+        fetch("https://api.coinpaprika.com/v1/tickers")
+        .then((res) => res.json())
+        .then((json)=> {
+            setCoins(json);
+            setLoading(false);
+        });
+    }, [])
+
+    return (
+    <>
+    <h1>The Coins ({coins.length})</h1>
+    {loading ? (<strong>Loading...</strong>) : (
+        <ul>
+            {coins.map((coin) => <li>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</li>)}
+        </ul>
+    )}
+    </>
+    )
+}
+
+export default C_TodoList;
