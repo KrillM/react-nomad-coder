0098d51932da3f9d2136cfe1536a6701cc7b4d04
krille <mks160615@gmail.com>
2026-07-07 21:59:19 +0900

    Revert "fetch"
    This reverts commit 0c4caced0711d967646cb97fd687619a836d0c27.

--- src/04_practice/PropsPractice.js
+++ src/04_practice/PropsPractice.js
@@ -1,10 +1,9 @@
 import A_TodoList from "./A_TodoList";
-import B_TodoList from "./B_TodoList";
 
 const PropsPractice = () => {
     return(
         <>
-        <B_TodoList/>
+        <A_TodoList />
         </>
     )
 } 
